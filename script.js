8c5b725f399022761a9c2f29fbaf2dae3bc44213
sam930518 <sam93051800@gmail.com>
2026-02-23 11:53:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gifStages = [
     "https://media1.tenor.com/m/BzB6arw6ChcAAAAC/lp.gif",    // 0 normal
-    "https://media.tenor.com/JNgxzvO2kxQAAAAj/%E7%B7%9A%E6%A2%9D%E5%B0%8F%E7%8B%97.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://media1.tenor.com/m/BzB6arw6ChcAAAAC/lp.gif",  // 1 confused
+    "https://media.tenor.com/JNgxzvO2kxQAAAAj/%E7%B7%9A%E6%A2%9D%E5%B0%8F%E7%8B%97.gif",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
